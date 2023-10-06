734b752bce5828dcea451181d764afac67407494
McMarquinez <mcmarquinez01@gmail.com>
2023-10-06 08:06:17 +0800

    TodosContoller crud function - working and tested via postman

--- app/Http/Controllers/ToDosController.php
+++ app/Http/Controllers/ToDosController.php
@@ -3,46 +3,63 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Task;
 
 class ToDosController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+    
     public function index()
     {
-        //
+        $tasks = Task::all();
+        return $tasks;
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+    
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'task' => 'required|string|max:255',
+        ]);
+
+        $task = Task::create([
+            'task' => $request->input('task'),
+        ]);
+
+        return $task;
     }
 
-    /**
-     * Display the specified resource.
-     */
+    
     public function show(string $id)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
+    
     public function update(Request $request, string $id)
     {
-        //
+        $task = Task::find($id);
+
+        if (!$task) {
+            return response()->json(['message' => 'Task not found'], 422);
+        }
+
+        $task->status = 'done';
+        $task->save();
+
+        return $task;
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
+    
     public function destroy(string $id)
     {
-        //
+        $task = Task::find($id);
+
+        if (!$task) {
+            return response()->json(['message' => 'Task not found'], 422);
+        }
+
+        $task->delete();
+
+        return response()->json(['message' => 'Task deleted successfully'], 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('todos', TodosController::class);
+Route::apiResource('tasks', TodosController::class);
